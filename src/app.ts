b5283f2f17ebb7f1e4d9a6de3a2183339aa1680b
Gregorio <gregoriocarranza@hotmail.com>
2024-06-20 14:32:58 -0300

    changes

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,17 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express, { Application } from "express";
 // import { MongoConfiguration } from "./configs/mongo.config";
-const morgan = require('morgan');
+const morgan = require("morgan");
 import { ChatRouter } from "./routes/chat/chat.routes";
 dotenv.config();
 
 // MongoConfiguration.connectDB();
 const app: Application = express();
 app.use(express.static("public"));
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(
   cors({
-    origin: process.env.CORS_ALLOWED_ORIGINS || "http://localhost:5500",
+    origin: "http://localhost:5173",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import WebsocketServices from "./websocket.service";
 const server: Server = createServer(app);
 const io: any = new SocketIO.Server(server, {
   cors: {
-    origin: process.env.CORS_ALLOWED_ORIGINS || "http://localhost:5500",
+    origin: "http://localhost:5173",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PUT", "DELETE"],
@@ -18,56 +18,26 @@ WebsocketServices._socketMap.set("io", io);
 const port: number = +process.env.PORT ?? 3000;
 const clientesConectados: any = [];
 
-// io.on("connection", (socket: any) => {
-//   console.log(`Cliente conectado ${socket.id}`);
-//   io.to(socket.id).emit("NEW_USER", clientesConectados);
-//   clientesConectados.push({ name: socket.id, key: socket.id });
-
-//   socket.on("chat message", (msg: any) => {
-//     console.log("Mensaje: " + msg);
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log(`-------Cliente desconectado------- ${socket.id}`);
-//     const index = clientesConectados.findIndex(
-//       (cliente: any) => cliente.key === socket.id
-//     );
-//     if (index !== -1) {
-//       clientesConectados.splice(index, 1);
-//     }
-
-//     io.emit("NEW_USER", clientesConectados);
-//   });
-// });
-
 io.on("connection", (socket: any) => {
-    console.log(`Cliente conectado ${socket.id}`);
-    
-    if (clientesConectados.length > 0) {
-      io.to(socket.id).emit("NEW_USER", clientesConectados);
+  console.log(`Cliente conectado ${socket.id}`);
+
+  const nuevoCliente = { key: socket.id, placeholder: socket.id };
+  clientesConectados.push(nuevoCliente);
+
+  io.emit("NEW_USER", clientesConectados);
+
+  socket.on("disconnect", () => {
+    console.log(`-------Cliente desconectado------- ${socket.id}`);
+    const index = clientesConectados.findIndex(
+      (cliente: any) => cliente.key === socket.id
+    );
+
+    if (index !== -1) {
+      clientesConectados.splice(index, 1);
+      io.emit("NEW_USER", clientesConectados);
     }
-    
-    const nuevoCliente = { name: socket.id, key: socket.id };
-    clientesConectados.push(nuevoCliente);
-  
-    socket.broadcast.emit("NEW_USER", [nuevoCliente]);
-  
-    // socket.on("chat message", (msg: any) => {
-    //   console.log("Mensaje: " + msg);
-    //   socket.broadcast.emit("chat message", msg);
-    // });
-  
-    socket.on("disconnect", () => {
-      console.log(`-------Cliente desconectado------- ${socket.id}`);
-      const index = clientesConectados.findIndex(
-        (cliente: any) => cliente.key === socket.id
-      );
-      if (index !== -1) {
-        clientesConectados.splice(index, 1);
-        io.emit("NEW_USER", clientesConectados);
-      }
-    });
   });
+});
 
 server.listen(port, () =>
   console.info(`Server up and running on port ${port}`)
